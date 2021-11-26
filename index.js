61271f9890b7fd490e362eb51cb8384bc7d6976c
hdyaro88 <hdyaro88@gmail.com>
2021-11-26 19:47:01 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 5000 , () => {
+    console.log("server is up at port : 5000")
+})
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+app.get("/api" , (req , res) => {
+    console.log("hello")
+    res.send({status : "hello , welcome"});
+})
