18c7e9033ca4b33fc893a894d32371ecb5b1e269
hdyaro88 <hdyaro88@gmail.com>
2021-11-27 02:12:32 +0530

    new3

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const path = require("path");
 app.listen(process.env.PORT || 5000 , () => {
     console.log("server is up at port : 5000")
 })
-app.use(express.static(path.join(__dirname, "/client/build")));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 app.get("/api" , (req , res) => {
     console.log("hello")
     res.send({status : "hello , welcome"});
